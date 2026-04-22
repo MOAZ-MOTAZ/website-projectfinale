129bf608338688faad431d8f10e0b600b14570cb
MOAZ-MOTAZ <moaazmoataz16@gmail.com>
2026-04-22 15:53:03 +0200

    Change Router to use hash location
    Updated Router component to use hash location for routing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -10,31 +11,23 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Teaser from "./pages/Teaser";
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
-    <Switch>
+    <Switch hook={useHashLocation}>
       <Route path={"/"} component={Teaser} />
       <Route path={"/lock"} component={Lock} />
       <Route path={"/home"} component={Home} />
       <Route path={"/admin"} component={AdminDashboard} />
       <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
       <Route component={NotFound} />
     </Switch>
   );
 }
-
-// NOTE: About Theme
-// - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
-//   to keep consistent foreground/background color across components
-// - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
 
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider
         defaultTheme="light"
-        // switchable
       >
         <TooltipProvider>
           <Toaster />
